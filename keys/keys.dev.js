2f388bc23893f013c87a0ab35ae29222ab6689c5
velzepooz <velzepooz@gmail.com>
2021-04-13 11:39:29 +0300

    Docommit

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,6 +1,5 @@
 module.exports = {
   // eslint-disable-next-line max-len
-  MONGO_DB_URL: 'mongodb+srv://gleb:Q2V5yJL4wksL7VT@cluster0.6uxeu.mongodb.net/users',
   SESSION_SECRET: 'some secret value',
   BASE_URL: 'http://localhost:9000',
   rpName: 'WebAuthApp',
